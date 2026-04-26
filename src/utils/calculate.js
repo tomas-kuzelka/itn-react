7bc0c2dd79019493150e36f79f186dec9d45c5b2
Tomas Kuzelka <tkuzelka@gmail.com>
2026-04-26 14:57:24 +0200

    utils

---
+++ src/utils/calculate.js
@@ -0,0 +1,24 @@
+
+
+export const operations = [
+    { value: "+", label: "Sčítání" },
+    { value: "-", label: "Odčítání" },
+    { value: "*", label: "Násobení" },
+    { value: "/", label: "Dělení" }
+];
+
+
+export function calculate(a, b, operation) {
+    switch (operation) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return (b != 0) ? a / b : null;
+        default:
+            return null;
+    }
+}
